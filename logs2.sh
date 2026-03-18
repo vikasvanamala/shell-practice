9ae6fd26b7d30fc02931554e729fd081f9c3563e
vikas-vanamala <vikasvanamala008@gmail.com>
2026-03-18 17:34:19 +0530

    first2

---
+++ logs2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+userid=$(id -u)
+
+logs_folder="/var/log/shell-script"
+scriptname=$( echo $0 | cut -d "." -f1 )
+log_file="$logs_folder/$scriptname.log"
+
+mkdir -p logs_folder 
+echo " script started excution at $(date)" | tee -a $logs_file
+
+if [ $userid -ne 0 ]; then
+    echo -e " $R Error: please run with root access $N" | tee -a $logs_file
+    exit 1;
+fi
+
+validate(){
+   if  [ $1 -ne 0 ]; then
+    echo -e "$R $2 installation failed $N" | tee -a $logs_file
+    exit 1
+   else
+    echo -e "$G $2 installation success $N" | tee -a $logs_file
+   fi 
+
+}
+
+dnf list installed mysql &>>$log_file
+if [ $? -ne 0 ]; then
+    dnf install mysql -y &>>$log_file
+    validate $? mysql
+else
+    echo -e "$Y mysql is already installed skipping... $N" | tee -a $logs_file
+fi    
+
+dnf list installed nginx &>>$log_file
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &>>$log_file
+    validate $? nginx 
+else
+    echo -e "$Y nginx is already installed skipping.. $N" | tee -a $logs_file
+fi        
