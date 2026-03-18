b3e7e7fc9b5563614bc3e30ce05285356adfe05f
vikas-vanamala <vikasvanamala008@gmail.com>
2026-03-18 16:50:08 +0530

    first2

---
+++ colors.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo -e " $G Error: please run with root access $N"
+    exit 1;
+fi
+
+validate(){
+   if  [ $1 -ne 0 ]; then
+    echo -e "$R $2 installation failed $N"
+    exit 1
+   else
+    echo -e "$Y $2 installation success $N"
+   fi 
+
+}
+
+dnf install mysql -y
+validate($? mysql)
+
+dnf install nginx -y
+validate($? nginx) 
