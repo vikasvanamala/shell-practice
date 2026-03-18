51a395b574c4bc30f343c7d5a42801956fbcbed5
vikas-vanamala <vikasvanamala008@gmail.com>
2026-03-18 15:51:52 +0530

    first2

--- datatypes.sh
+++ datatypes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-heroes=("nani","prabhas","raviteja")
+heroes=("nani" "prabhas" "raviteja")
 echo "all heroes names: ${heroes[@]}"
 echo "my favourite hero name is: ${heroes[0]}"
 echo "pan indis heroes: ${heroes[1,2]}"
