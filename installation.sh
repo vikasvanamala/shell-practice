c27915c8df913da3d9c7d4a2b869f123c4ff1a53
vikas-vanamala <vikasvanamala008@gmail.com>
2026-03-18 16:23:31 +0530

    first2

--- installation.sh
+++ installation.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "Error: please run this with root access"
+    exit 1
+    
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "sql installation failed"
+    exit 1
 else
     echo "mysql installation is success"
 fi         
