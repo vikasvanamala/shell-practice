dda7faee471b0f8ecb19453be64e7c952b6a8c45
vikas-vanamala <vikasvanamala008@gmail.com>
2026-03-25 18:51:45 +0530

    df

--- shell-assignments/file-exists.sh
+++ shell-assignments/file-exists.sh
@@ -6,11 +6,11 @@ filename=$1
 if [ -e "$filename" ];then
     echo "$filename exists"
 
-    if [ -r "$filename"]; then
+    if [ -r "$filename" ]; then
         echo $filename is readable
     fi   
 
-    if [ -w "$filenmae"]. then
+    if [ -w "$filenmae" ]; then
         echo "file is writable 
     fi    
 
